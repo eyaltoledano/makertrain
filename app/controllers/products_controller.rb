18a54e0e0e0fbfb3d56671a29198feaf68e36776
Eyal Toledano <eutait@gmail.com>
2019-08-08 21:58:04 -0400

    Adds product creation form and controller logic.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,34 @@
 class ProductsController < ApplicationController
   def index
     set_current_user
+    @products = Product.all
   end
 
   def new
     set_current_user
     redirect_if_not_logged_in
+    @product = @user.products.build
+    @version = @product.versions.build(version_number: "v1")
   end
 
   def create
     set_current_user
-    redirect_if_not_logged_in
+    if product_params[:name] == "" || product_params[:description] == "" || product_params[:git_repo] == ""
+      flash[:notice] = "You're missing some information. Let's try again."
+      render :new
+    else
+      product_params[:name].chomp
+      @product = @user.products.build(product_params)
+      @product.status = "New"
+      @product.save
+      flash[:notice] = "#{@product.name} was successfully created!"
+      redirect_to product_path(@product)
+    end
   end
 
   def show
+    set_current_user
+    @product = Product.find(params[:id])
   end
 
   def edit
@@ -26,4 +41,11 @@ class ProductsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :website, :git_repo, :user_id)
+  end
+
 end
