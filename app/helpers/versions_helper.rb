82f30a10e6b1a4d893cab7b09a3e75b68e1ebb8b
Eyal Toledano <eutait@gmail.com>
2019-08-15 15:55:49 -0400

    Adds progress bar partial aided by progress_hash(version), progress_hash_numbers(version), progress_bar_class(status) and progress_bar_popover_content(version, task_status) version helpers to populate the values of the version progress bar.

--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -1,20 +1,46 @@
 module VersionsHelper
 
+  def progress_hash_numbers(version)
+    status_hash = {available: [], in_progress: [], in_review: [], complete: []}
+    # First, populate the status_hash with tasks fitting into the statuses
+    version.tasks.each do |task|
+
+      if task.status == "New" || task.status == "Open"
+        status_hash[:available] << task
+      elsif task.status ==  "Claimed" || task.status == "Researching" || task.status == "Writing specs" || task.status == "In progress" || task.status == "Ready for Review"
+        status_hash[:in_progress] << task
+      elsif task.status == "Reviewing" || task.status == "Accepted" || task.status == "Rejected"
+        status_hash[:in_review] << task
+      elsif task.status == "Completed"
+        status_hash[:complete] << task
+      end
+    end
+
+    # Then, replace the value of each status with the count of how many values there are in each status_hash key
+    status_hash = status_hash.each do |status, task_list|
+      status_hash[status] = task_list.size
+    end
+    # Count how many tasks there are in total by summing up all the keys' values
+    total_tasks = status_hash.values.inject(0) {|a,b|a+b}
+    status_hash
+  end
+
   def progress_hash(version)
     status_hash = {available: [], in_progress: [], in_review: [], complete: []}
     # First, populate the status_hash with tasks fitting into the statuses
     version.tasks.each do |task|
-      case task.status
-        when "New"
-          status_hash[:available] << task
-        when "Researching" || "Writing specs" || "In progress" || "Ready for Review"
-          status_hash[:in_progress] << task
-        when "Reviewing" || "Accepted" || "Rejected"
-          status_hash[:in_review] << task
-        when "Completed"
-          status_hash[:complete] << task
+
+      if task.status == "New" || task.status == "Open"
+        status_hash[:available] << task
+      elsif task.status ==  "Claimed" || task.status == "Researching" || task.status == "Writing specs" || task.status == "In progress" || task.status == "Ready for Review"
+        status_hash[:in_progress] << task
+      elsif task.status == "Reviewing" || task.status == "Accepted" || task.status == "Rejected"
+        status_hash[:in_review] << task
+      elsif task.status == "Completed"
+        status_hash[:complete] << task
       end
     end
+
     # Then, replace the value of each status with the count of how many values there are in each status_hash key
     status_hash = status_hash.each do |status, task_list|
       status_hash[status] = task_list.size
@@ -32,6 +58,8 @@ module VersionsHelper
     # progress_hash {:available=>0.8, :in_progress=>0.1, :in_review=>0.05, :complete=>0.05}
   end
 
+
+
   def progress_bar_class(status)
     case status
     when :available
@@ -41,6 +69,10 @@ module VersionsHelper
     when :complete
       "progress-bar-success"
     end
+  end
+
+  def progress_bar_popover_content(version, task_status)
+    "#{pluralize(progress_hash_numbers(version)[task_status], 'Task')} #{task_status.to_s.capitalize.humanize.titleize}"
   end
 
 end
