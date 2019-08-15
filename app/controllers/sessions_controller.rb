d7acaa687ee525f057eea5ca775c098e3078c05d
Eyal Toledano <eutait@gmail.com>
2019-08-15 01:37:33 -0400

    Solves an issue whereby username and/or image was not set due to how the GitHub and ProductHunt APIs differ. This was rectified by adding if statements to the sessions#create action. Also adds a redirect upon account creation via oauth to set a display name and set a password (as one was automatically generated during oauth).

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,15 +12,25 @@ class SessionsController < ApplicationController
       else
         # new user via oauth
         @user = User.create do |u|
-          u.username = auth[:info][:username]
+          if auth[:info][:nickname].present?
+            u.username = auth[:info][:nickname]
+          else
+            u.username = auth[:info][:username]
+          end
           u.email = auth[:info][:email]
-          u.image = auth[:info][:image]
+          binding.pry
+          if auth[:info][:image].present?
+            u.image = auth[:info][:image]
+          else
+            u.image = auth[:info][:avatar]
+          end
           u.uid = auth[:uid]
           # set a random password for new oauth users
           u.password = SecureRandom.hex
         end
         session[:user_id] = @user.id
-        redirect_to dashboard_path
+        flash[:notice] = "Welcome to Makertrain! You should take a second to set a password and a custom display name if you'd like one."
+        redirect_to edit_user_path(@user)
       end
     else
       # normal login via login form (login with email or username)
